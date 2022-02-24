2235cf5a927d2b61b817c718a024ce149b9826da
Davide Pavani <pavani.davide98@gmail.com>
2022-02-24 18:03:24 +0100

    Added Carousel and NavigationDrawer

--- Tengu/ViewModels/MainWindowViewModel.cs
+++ Tengu/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,10 @@ namespace Tengu.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+
+        public MainWindowViewModel()
+        {
+
+        }
     }
 }
