4497f38926dcc0e92031490b0eef1bac52ae07d9
Davide Pavani <d.pavani@maticacorp.com>
2022-02-28 14:05:13 +0100

    Changed ItemsControl item panel

--- Tengu/ViewModels/LatestEpisodesPageViewModel.cs
+++ Tengu/ViewModels/LatestEpisodesPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Tengu.Enums;
 using Tengu.Extensions;
 using Tengu.Utilities;
@@ -15,13 +16,32 @@ namespace Tengu.ViewModels
         private CustomObservableCollection<string> animeList = new(); // MODEL
         private TenguHost selectedHost = TenguHost.AnimeSaturn;
         private bool loadingAnimes = false;
+        private int currentPage = 0;
+
+        private bool canPrev = false;
 
         #region Properties
+        public ICommand NextPageCommand { get; private set; }
+        public ICommand PrevPageCommand { get; private set; }
         public List<TenguHost> HostList { get; private set; }
+        public bool CanPrev
+        {
+            get => canPrev;
+            set => this.RaiseAndSetIfChanged(ref canPrev, value);
+        }
         public bool LoadingAnimes
         {
             get => loadingAnimes;
             set => this.RaiseAndSetIfChanged(ref loadingAnimes, value);
+        }
+        public int CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref currentPage, value);
+                CanPrev = !value.Equals(0);
+            }
         }
         public CustomObservableCollection<string> AnimeList
         {
@@ -44,6 +64,25 @@ namespace Tengu.ViewModels
         {
             HostList = EnumExtension.ToList<TenguHost>();
 
+            AnimeList.Add("123");
+            AnimeList.Add("123");
+            AnimeList.Add("123");
+            AnimeList.Add("123");
+            AnimeList.Add("123"); 
+            AnimeList.Add("123");
+            AnimeList.Add("123");
+
+            NextPageCommand = ReactiveCommand.Create(NextPage);
+            PrevPageCommand = ReactiveCommand.Create(PrevPage);
+        }
+
+        public void PrevPage()
+        {
+
+        }
+        public void NextPage()
+        {
+
         }
 
         private void ReloadAnimes()
@@ -51,6 +90,7 @@ namespace Tengu.ViewModels
             Task.Run(() =>
             {
                 LoadingAnimes = true;
+                CurrentPage = 0;
 
                 AnimeList.Clear();
 
@@ -61,6 +101,5 @@ namespace Tengu.ViewModels
                 LoadingAnimes = false;
             });
         }
-
     }
 }
