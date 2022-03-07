476ab4c4413be984d0625a385b96eb0724f365a9
Davide Pavani <d.pavani@maticacorp.com>
2022-03-07 09:56:35 +0100

    Adde Project ling working

--- Tengu/ViewModels/HomePageViewModel.cs
+++ Tengu/ViewModels/HomePageViewModel.cs
@@ -5,6 +5,7 @@ using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Input;
 
@@ -25,7 +26,7 @@ namespace Tengu.ViewModels
             SetDarkModeCommand = ReactiveCommand.Create(UseMaterialUIDarkTheme);
             SetLightModeCommand = ReactiveCommand.Create(UseMaterialUILightTheme);
 
-            OpenProjectLinkCommand = ReactiveCommand.Create(OpenGitHubProject);
+            OpenProjectLinkCommand = ReactiveCommand.Create<string>(OpenGitHubProject);
         }
 
         public void UseMaterialUIDarkTheme()
@@ -34,7 +35,39 @@ namespace Tengu.ViewModels
         public void UseMaterialUILightTheme()
             => MaterialThemeStyles.BaseTheme = BaseThemeMode.Light;
 
-        public void OpenGitHubProject()
-            => Process.Start("https://github.com/Dugongoo/Tengu");
+        public void OpenGitHubProject(string project)
+        {
+            string url = project switch
+            {
+                "Tengu.Business" => "https://github.com/giuseppeSalerno10/Tengu.Business",
+                _ => "https://github.com/Dugongoo/Tengu",
+            };
+
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    // Logging
+                }
+            }
+        }
     }
 }
