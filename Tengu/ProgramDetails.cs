93488b7b674885896f8ad268a721a2610a74763c
Davide Pavani <d.pavani@maticacorp.com>
2022-03-14 11:48:18 +0100

    added versioning

---
+++ Tengu/ProgramDetails.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tengu
+{
+    public static class ProgramDetails
+    {
+        public const string ApplicationName = @"Tengu";
+        public const string ApplicationDisplay = @"Tengu";
+
+        public const string CompanyName = @"Tengu Business";
+
+        private const string MajorVersion = "1"; // Manual Increment
+        private const string MinorVersion = "0"; // Manual Increment
+        private const string BuildVersion = "*"; // Auto Increment
+
+        // AssemblyVersion Property
+        public const string FullVersion = $"{MajorVersion}.{MinorVersion}.{BuildVersion}";
+
+        // REAL Version Assembly 
+        private static Version VersionAssembly => Assembly.GetExecutingAssembly().GetName().Version;
+
+        #region Properties
+
+        /// <summary>
+        /// Build Date and Time
+        /// </summary>
+        public static string BuildDate => $"{new DateTime(2000, 1, 1).AddDays(VersionAssembly.Build).AddSeconds(VersionAssembly.Revision * 2)}";
+
+        /// <summary>
+        /// Display Application Version (Revision Version if Debug Mode)
+        /// </summary>
+        public static string DisplayVersion
+        {
+            get
+            {
+                return $"{VersionAssembly.Major}" +
+                       $".{VersionAssembly.Minor.ToString().PadLeft(2, '0')}" +
+                       $".{VersionAssembly.Build.ToString().PadLeft(2, '0')}"
+#if DEBUG
+                     + $" (Rev {VersionAssembly.Revision.ToString().PadLeft(2, '0')})"
+#endif
+                 ;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Tengu/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using Tengu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: AssemblyTitle(ProgramDetails.ApplicationName)]
+[assembly: AssemblyCompany(ProgramDetails.CompanyName)]
+[assembly: AssemblyProduct(ProgramDetails.ApplicationName)]
+
+[assembly: AssemblyVersion(ProgramDetails.FullVersion)]
+
+#pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
+[assembly: AssemblyFileVersion(ProgramDetails.FullVersion)]
+#pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
