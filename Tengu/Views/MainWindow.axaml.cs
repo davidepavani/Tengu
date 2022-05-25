4efc0922add1ce7c17d317001bbe3b096c59e550
Davide Pavani <pavani.davide98@gmail.com>
2022-05-25 20:53:18 +0200

    small changes

--- Tengu/Views/MainWindow.axaml.cs
+++ Tengu/Views/MainWindow.axaml.cs
@@ -1,8 +1,12 @@
+using Avalonia;
 using Avalonia.Controls;
+using FluentAvalonia.Core.ApplicationModel;
+using FluentAvalonia.UI.Controls;
+using System;
 
 namespace Tengu.Views
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : CoreWindow
     {
         public MainWindow()
         {
@@ -10,6 +14,33 @@ namespace Tengu.Views
 
             MinWidth = 600;
             MinHeight = 500;
+
+            Opened += OnParentWindowOpened;
+        }
+
+        private void OnParentWindowOpened(object sender, EventArgs e)
+        {
+            var titleBar = this.TitleBar;
+            if (titleBar != null)
+            {
+                titleBar.ExtendViewIntoTitleBar = true;
+
+                titleBar.LayoutMetricsChanged += OnApplicationTitleBarLayoutMetricsChanged;
+
+                if (this.FindControl<Grid>("TitleBarHost") is Grid g)
+                {
+                    SetTitleBar(g);
+                    g.Margin = new Thickness(0, 0, titleBar.SystemOverlayRightInset, 0);
+                }
+            }
+        }
+
+        private void OnApplicationTitleBarLayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (this.FindControl<Grid>("TitleBarHost") is Grid g)
+            {
+                g.Margin = new Thickness(0, 0, sender.SystemOverlayRightInset, 0);
+            }
         }
     }
 }
