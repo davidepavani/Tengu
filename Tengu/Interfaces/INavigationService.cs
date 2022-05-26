183dc578adf0e66434085ea92a24f9c09c30e200
Davide Pavani <d.pavani@maticacorp.com>
2022-05-26 09:45:00 +0200

    added navigation service

---
+++ Tengu/Interfaces/INavigationService.cs
@@ -0,0 +1,19 @@
+﻿using FluentAvalonia.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tengu.Interfaces
+{
+    public interface INavigationService
+    {
+        Frame NavigationFrame { get; }
+        bool CanGoBack { get; }
+        void SetFrame(Frame frame);
+        void Navigate(Type t);
+        void Navigate(Type t, object parameter);
+        void GoBack();
+    }
+}
