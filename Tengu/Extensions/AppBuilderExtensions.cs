c832aab7d77225002e9948a999b12caa3e167c30
Davide Pavani <d.pavani@maticacorp.com>
2022-05-30 15:48:54 +0200

    added app builder extension

--- Tengu/Extensions/AppBuilderExtensions.cs
+++ Tengu/Extensions/AppBuilderExtensions.cs
@@ -1,10 +1,13 @@
 ﻿using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Splat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tengu.Business.API;
 using Tengu.Interfaces;
 using Tengu.Services;
 
@@ -26,6 +29,27 @@ namespace Tengu.Extensions
 
                         // Navigation Service
                         Locator.CurrentMutable.RegisterConstant(new NavigationService(), typeof(INavigationService));
+                    }
+                });
+            });
+        }
+
+        public static TAppBuilder UseTenguApi<TAppBuilder>(this TAppBuilder builder) where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
+        {
+            return builder.AfterPlatformServicesSetup(delegate
+            {
+                Locator.RegisterResolverCallbackChanged(delegate
+                {
+                    if (Locator.CurrentMutable != null)
+                    {
+                        using IHost host = Host.CreateDefaultBuilder()
+                                 .ConfigureServices((_, services) =>
+                                     services.AddTenguServices())
+                                 .Build();
+
+                        TenguApi tenguApi = (TenguApi)ActivatorUtilities.CreateInstance(host.Services, typeof(TenguApi));
+
+                        Locator.CurrentMutable.RegisterConstant(tenguApi, typeof(ITenguApi));
                     }
                 });
             });
