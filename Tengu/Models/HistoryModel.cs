5f585ef2c71835d5e47d05131aecb66f3b2f47e3
Davide Pavani <d.pavani@maticacorp.com>
2022-05-30 15:48:42 +0200

    added download models

---
+++ Tengu/Models/HistoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tengu.Models
+{
+    public class HistoryModel
+    {
+        public string Name { get; set; }
+        public string Episode { get; set; }
+        // HOST
+        public string ErrorMessage { get; set; }
+        public bool InError { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
