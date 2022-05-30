293c3fa7267ae1a973f7648a803764b9527fa170
Davide Pavani <pavani.davide98@gmail.com>
2022-05-30 22:14:14 +0200

    moved properties in Latest episodes control and changed ui

--- Tengu/Services/TenguManagerService.cs
+++ Tengu/Services/TenguManagerService.cs
@@ -13,65 +13,6 @@ namespace Tengu.Services
 {
     public class TenguManagerService : ReactiveObject
     {
-        private AvaloniaList<EpisodeModel> latestEpisodesList = new();
-        private int latestEpisodesOffset = 0;
-
-        private bool loading = false;
-
-        private static ITenguApi tenguApi => Locator.Current.GetService<ITenguApi>();
-
-        public AvaloniaList<EpisodeModel> LatestEpisodesList
-        {
-            get => latestEpisodesList;
-            set => this.RaiseAndSetIfChanged(ref latestEpisodesList, value);
-        }
-        public int LatestEpisodesOffset
-        {
-            get => latestEpisodesOffset;
-            set => this.RaiseAndSetIfChanged(ref latestEpisodesOffset, value);
-        }
-        public bool Loading
-        {
-            get => loading;
-            set => this.RaiseAndSetIfChanged(ref loading, value);
-        }
-
-        public void LatestNextPage()
-        {
-            LatestEpisodesOffset += 10;
-            // log.Trace($"Next page >> {CurrentPage}");
-
-            RefreshLatestEpisodes();
-        }
-
-        public void LatestPrevPage()
-        {
-            if (LatestEpisodesOffset >= 10) 
-            {
-                LatestEpisodesOffset -= 10;
-                // log.Trace($"Prev page >> {CurrentPage}");
-
-                RefreshLatestEpisodes();
-            }
-        }
-
-        private async void RefreshLatestEpisodes()
-        {
-            Loading = true;
-
-            try
-            {
-                LatestEpisodesList.Clear();
-
-                foreach (EpisodeModel episode in await tenguApi.GetLatestEpisodeAsync(LatestEpisodesOffset, LatestEpisodesOffset + 10))
-                {
-                    LatestEpisodesList.Add(episode);
-                }
-            }
-            finally
-            {
-                Loading = false;
-            }
-        }
+        
     }
 }
--- Tengu/ViewModels/LatestEpisodesControlViewModel.cs
+++ Tengu/ViewModels/LatestEpisodesControlViewModel.cs
@@ -1,19 +1,84 @@
-﻿using System;
+﻿using Avalonia.Collections;
+using ReactiveUI;
+using Splat;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tengu.Business.API;
+using Tengu.Business.Commons;
 
 namespace Tengu.ViewModels
 {
     public class LatestEpisodesControlViewModel : ViewModelBase
     {
-        public List<string> Episodes { get; set; } = new()
+        private AvaloniaList<EpisodeModel> latestEpisodesList = new();
+        private int latestEpisodesOffset = 0;
+
+        private bool loading = false;
+
+        private readonly ITenguApi tenguApi;
+
+        #region Properties
+        public AvaloniaList<EpisodeModel> LatestEpisodesList
         {
-            "Episode1",
-            "Episode2",
-            "Episode3",
-            "Episode4",
-        };
+            get => latestEpisodesList;
+            set => this.RaiseAndSetIfChanged(ref latestEpisodesList, value);
+        }
+        public int LatestEpisodesOffset
+        {
+            get => latestEpisodesOffset;
+            set => this.RaiseAndSetIfChanged(ref latestEpisodesOffset, value);
+        }
+        public bool Loading
+        {
+            get => loading;
+            set => this.RaiseAndSetIfChanged(ref loading, value);
+        }
+        #endregion
+
+        public LatestEpisodesControlViewModel()
+        {
+            tenguApi = Locator.Current.GetService<ITenguApi>();
+        }
+
+        public void LatestNextPage()
+        {
+            LatestEpisodesOffset += 10;
+            // log.Trace($"Next page >> {CurrentPage}");
+
+            RefreshLatestEpisodes();
+        }
+
+        public void LatestPrevPage()
+        {
+            if (LatestEpisodesOffset >= 10)
+            {
+                LatestEpisodesOffset -= 10;
+                // log.Trace($"Prev page >> {CurrentPage}");
+
+                RefreshLatestEpisodes();
+            }
+        }
+
+        private async void RefreshLatestEpisodes()
+        {
+            Loading = true;
+
+            try
+            {
+                LatestEpisodesList.Clear();
+
+                foreach (EpisodeModel episode in await tenguApi.GetLatestEpisodeAsync(LatestEpisodesOffset, LatestEpisodesOffset + 10))
+                {
+                    LatestEpisodesList.Add(episode);
+                }
+            }
+            finally
+            {
+                Loading = false;
+            }
+        }
     }
 }
