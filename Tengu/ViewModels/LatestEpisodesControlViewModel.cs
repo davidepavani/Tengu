efba3fc5607def99bc594ac6a9f5076f95b95293
Davide Pavani <pavani.davide98@gmail.com>
2022-05-31 18:50:37 +0200

    added hosts and initialization of latest episodes

--- Tengu/ViewModels/LatestEpisodesControlViewModel.cs
+++ Tengu/ViewModels/LatestEpisodesControlViewModel.cs
@@ -15,51 +15,65 @@ namespace Tengu.ViewModels
     {
         private AvaloniaList<EpisodeModel> latestEpisodesList = new();
         private int latestEpisodesOffset = 0;
+        private Hosts selectedHost = Hosts.AnimeSaturn;
 
         private bool loading = false;
+        private bool canPrev = false;
 
         private readonly ITenguApi tenguApi;
 
         #region Properties
+        public List<Hosts> HostsList { get; private set; }
         public AvaloniaList<EpisodeModel> LatestEpisodesList
         {
             get => latestEpisodesList;
             set => this.RaiseAndSetIfChanged(ref latestEpisodesList, value);
         }
+        public Hosts SelectedHost
+        {
+            get => selectedHost;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref selectedHost, value);
+                Initialize();
+            }
+        }
         public int LatestEpisodesOffset
         {
             get => latestEpisodesOffset;
-            set => this.RaiseAndSetIfChanged(ref latestEpisodesOffset, value);
+            set
+            {
+                this.RaiseAndSetIfChanged(ref latestEpisodesOffset, value);
+                CanPrev = value > 10;
+            }
         }
         public bool Loading
         {
             get => loading;
             set => this.RaiseAndSetIfChanged(ref loading, value);
         }
+        public bool CanPrev
+        {
+            get => canPrev;
+            set => this.RaiseAndSetIfChanged(ref canPrev, value);
+        }
         #endregion
 
         public LatestEpisodesControlViewModel()
         {
             tenguApi = Locator.Current.GetService<ITenguApi>();
+            HostsList = Enum.GetValues(typeof(Hosts)).Cast<Hosts>().ToList();
         }
 
-        public void LatestNextPage()
+        private void Initialize()
         {
-            LatestEpisodesOffset += 10;
-            // log.Trace($"Next page >> {CurrentPage}");
+            tenguApi.CurrentHosts = new Hosts[] { SelectedHost };
+            LatestEpisodesOffset = 0;
+
+            //ImageWidth = host.Equals(Hosts.AnimeUnity) ? 150 : 377.9;
+            //BorderWidth = ImageWidth + 20;
 
             RefreshLatestEpisodes();
-        }
-
-        public void LatestPrevPage()
-        {
-            if (LatestEpisodesOffset >= 10)
-            {
-                LatestEpisodesOffset -= 10;
-                // log.Trace($"Prev page >> {CurrentPage}");
-
-                RefreshLatestEpisodes();
-            }
         }
 
         private async void RefreshLatestEpisodes()
@@ -78,6 +92,25 @@ namespace Tengu.ViewModels
             finally
             {
                 Loading = false;
+            }
+        }
+
+        public void LatestNextPage()
+        {
+            LatestEpisodesOffset += 10;
+            // log.Trace($"Next page >> {CurrentPage}");
+
+            RefreshLatestEpisodes();
+        }
+
+        public void LatestPrevPage()
+        {
+            if (LatestEpisodesOffset >= 10)
+            {
+                LatestEpisodesOffset -= 10;
+                // log.Trace($"Prev page >> {CurrentPage}");
+
+                RefreshLatestEpisodes();
             }
         }
     }
