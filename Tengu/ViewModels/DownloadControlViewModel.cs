d224c8bec88ced62d345f751e0938e1838987cdf
Davide Pavani <d.pavani@maticacorp.com>
2022-06-17 11:11:58 +0200

    small changes on pane

--- Tengu/ViewModels/DownloadControlViewModel.cs
+++ Tengu/ViewModels/DownloadControlViewModel.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Tengu.ViewModels
 {
     public class DownloadControlViewModel : ViewModelBase
     {
+        private bool isPaneOpen = false;
+
+        public ICommand OpenPaneCommand { get; set; }
+        public bool IsPaneOpen 
+        { 
+            get => isPaneOpen;
+            set => this.RaiseAndSetIfChanged(ref isPaneOpen, value);
+        }
+
         public List<string> Downloads { get; set; } = new()
         {
             "downloadaaaaaaaaaaaaaaaaaaaaaaaaa 1",
@@ -18,7 +29,7 @@ namespace Tengu.ViewModels
 
         public DownloadControlViewModel()
         {
-
+            OpenPaneCommand = ReactiveCommand.Create(() => IsPaneOpen = true);
         }
     }
 }
