8ec210cd21c8d32188c0e7882eb23ebd989031c8
Davide Pavani <pavani.davide98@gmail.com>
2022-07-05 19:24:41 +0200

    fixed json config app color load

--- Tengu/Models/CustomColorModel.cs
+++ Tengu/Models/CustomColorModel.cs
@@ -4,14 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tengu.Enums;
+using Tengu.Extensions;
 
 namespace Tengu.Models
 {
     public class CustomColorModel
     {
-        public string Name { get; private set; }
-        public string Hex { get; private set; }
+        public string Name { get; set; }
+        public string Hex { get; set; }
 
+        public CustomColorModel() : this(DefaultColors.darkslateblue.GetEnumDescription(), $"#FF{(uint)DefaultColors.darkslateblue:x6}") { }
         public CustomColorModel(string name, string hex)
         {
             Name = name;
