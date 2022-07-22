c4f8ddb8aa49c7ee3d4340485bb33eb28a3ac301
Davide Pavani <d.pavani@maticacorp.com>
2022-07-22 09:25:29 +0200

    ui changes and news in home page

---
+++ Tengu/Models/NewModel.cs
@@ -0,0 +1,20 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tengu.Models
+{
+    public class NewModel : ReactiveObject
+    {
+        public string Category { get; private set; } = string.Empty;
+        public List<string> News { get; set; } = new();
+
+        public NewModel(string category)
+        {
+            Category = category;
+        }
+    }
+}
--- Tengu/Views/HomePageControl.axaml.cs
+++ Tengu/Views/HomePageControl.axaml.cs
@@ -1,14 +1,26 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using Tengu.ViewModels;
 
 namespace Tengu.Views
 {
-    public partial class HomePageControl : UserControl
+    public partial class HomePageControl : ReactiveUserControl<HomePageControlViewModel>
     {
         public HomePageControl()
         {
             InitializeComponent();
+        }
+
+        private void HlbNavigateLatest_Click(object sender, RoutedEventArgs args) 
+        {
+            ViewModel.Navigator.Navigate(typeof(LatestEpisodesControl));
+        }
+        private void HlbNavigateSearch_Click(object sender, RoutedEventArgs args)
+        {
+            ViewModel.Navigator.Navigate(typeof(SearchControl));
         }
 
         private void InitializeComponent()
