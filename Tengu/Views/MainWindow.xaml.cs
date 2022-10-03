0d2d1296d2a1c7aa93094e22a081fe11590703e3
Davide Pavani <d.pavani@maticacorp.com>
2022-10-03 14:07:27 +0200

    added hancycontrol and moved MainWindow insive ViewsFolder

--- Tengu/Views/MainWindow.xaml.cs
+++ Tengu/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Tengu
+namespace Tengu.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
